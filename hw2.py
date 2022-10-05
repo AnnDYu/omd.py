9d2881844d1c4e421a7ff5ced659f30dd01f1a2a
Anna Dyatlova <anna02092014@mail.com>
2022-10-05 12:33:52 +0300

    aaaa

--- hw2.py
+++ hw2.py
@@ -1,20 +1,21 @@
 
-""" функция преобразует csv файл в текст и собирает в массив, где каждый элемент - массив с данными о работнике"""
 def csv_to_list(csv_file: str) -> list:
+    """функция преобразует csv файл в текст и собирает в массив, где каждый элемент - массив с данными
+     о работнике"""
     with open(csv_file, 'r', encoding='utf8') as file:
         lines = file.readlines()
-        head = lines[0].split(";")
+        head = lines[0].split(';')
         lines.pop(0)
         data = []
-        for line in range(len(lines)):
-            info = lines[line].split(";")
+        for line in lines:
+            info = line.split(';')
             data_dict = dict(zip(head, info))
             data.append(data_dict)
-            line = line + 1
     return data
 
-""" функция выполняет одну из трех выбранных команд"""
+
 def question(data: list):
+    """ функция выполняет одну из трех выбранных команд"""
     print(f'Возможности: 1) Показать иерархию команд. 2) Вывести сводный отчёт по департаментам. '
           f'3) Сохранить сводный отчёт в csv-файл.'
           )
@@ -25,9 +26,10 @@ def question(data: list):
         option = input()
     return options[option](data)
 
-"""функция состовляет список из департаментов (без повторяющихся элементов), затем составляет список из словарей,
- где ключи - департаменты, а значения - его отделы"""
+
 def hierarchy(data: list) -> dict:
+    """функция состовляет список из департаментов (без повторяющихся элементов), затем составляет список из словарей,
+     где ключи - департаменты, а значения - его отделы"""
     depart = []
     for row in data:
         depart.append(row['Департамент'])
@@ -38,8 +40,9 @@ def hierarchy(data: list) -> dict:
             dep_kom[row['Департамент']].append(row['Отдел'])
     print(dep_kom)
 
-""" функция подсчитывает в каждом отделе кол-во работников, мин, макс, среднюю зарплату """
+
 def display_report(data: list) -> list:
+    """ функция подсчитывает в каждом отделе кол-во работников, мин, макс, среднюю зарплату """
     depart = []
     for row in data:
         depart.append(row['Департамент'])
@@ -65,8 +68,9 @@ def display_report(data: list) -> list:
     print(final)
     return final
 
-""" функция сохраняет отчет о значениях по департаментам в csv формате"""
+
 def save_to_csv(data: list):
+    """ функция сохраняет отчет о значениях по департаментам в csv формате"""
     final = display_report(data)
     head = ['Департамент', 'Кол-во работников', 'Макс зп', 'Мин зп', 'Средняя зп']
     final = [head] + final
