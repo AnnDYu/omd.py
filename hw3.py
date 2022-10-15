5d6d6c46f42de68387588c8b4b3cf76386e9fe14
Anna Dyatlova <anna02092014@mail.com>
2022-10-15 19:41:59 +0300

    aaaa

--- hw3.py
+++ hw3.py
@@ -1,32 +1,55 @@
-corpus = [
-    'Crock Pot Pasta Never boil pasta again',
-    'Pasta Pomodoro Fresh ingredients Parmesan to taste'
-]
-
-
 class CountVectorizer:
-    def __init__(self,corpus):
+    def __init__(self):
+        self.words = []
+        self.matrix = []
+        self.termfreq = []
+        pass
 
+    def fit_transform(self, some_text: list[str]) -> list:
+        mat=[]
+        for sentence in some_text:
+            one = sentence.split()
+            for word in one:
+                word = word.lower()
+                if word not in self.words:
+                    self.words.append(word)
+        for sentence in some_text:
+            one = str(sentence.split()).lower()
+            for uniqword in self.words:
+                number = one.count(uniqword)
+                mat.append(number)
+            self.matrix.append(mat)
+            mat = []
+        return self.matrix
 
-    def get_feature_names(self,corpus) -> list:
-        word_matrix = []
-        for sentences in self:
-            word_matrix.append(sentences.strip().split(' '))
-        word_matr = []
-        for i in word_matrix:
-            word_matr = word_matr + i
-        return list(set(word_matr))
+    def get_feature_names(self) -> list[str]:
+        return self.words
 
-    def fit_transform(self,corpus) -> list:
+    if __name__ == '__main__':
+        corpus = [
+            'Crock Pot Pasta Never boil pasta again',
+            'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+        ]
+        vectorizer = CountVectorizer()
+        count_matrix = vectorizer.fit_transform(corpus)
+        print(vectorizer.get_feature_names())
+        print(count_matrix)
 
-        for elements in corpus:
-            words = elements.lower().split()
-            mass=[]
-            for sent in self.get_feature_names(corpus):
-                num_words = words.count(sent)
-                mass.append(num_words)
-            fin.append(mass)
+    # def get_feature_names(self,corpus) -> list:
+    # word_matrix = []
+    # for sentences in self:
+    # word_matrix.append(sentences.strip().split(' '))
+    # word_matr = []
+    # for i in word_matrix:
+    # word_matr = word_matr + i
+    # return list(set(word_matr))
 
-vectorizer = CountVectorizer()
-count_matrix = vectorizer.fit_transform
-print(count_matrix)
+    # def fit_transform(self,corpus) -> list:
+
+    # for elements in corpus:
+    # words = elements.lower().split()
+    # mass=[]
+    # for sent in self.get_feature_names(corpus):
+    # num_words = words.count(sent)
+    # mass.append(num_words)
+    # fin.append(mass)
