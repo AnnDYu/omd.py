4114cbae3722634dac26c18d45094226a0f1aac3
Anna Dyatlova <anna02092014@mail.com>
2022-11-28 15:21:15 +0300

    aaaa

--- hw3.py
+++ hw3.py
@@ -1,10 +1,11 @@
 class CountVectorizer:
+    """преобразовывает массив с предложениями в 2 матрицы"""
     def __init__(self):
         self.words = []
         self.matrix = []
-        pass
 
     def fit_transform(self, some_text: list[str]) -> list:
+        """преобразует начальный массив в массив из массивов с колличеством слов для каждого предложения"""
         mat = []
         for sentence in some_text:
             one = sentence.split()
@@ -22,33 +23,16 @@ class CountVectorizer:
         return self.matrix
 
     def get_feature_names(self) -> list[str]:
+        """возвращает массив уникальных слов из всех предложений"""
         return self.words
 
-    if __name__ == '__main__':
-        corpus = [
-            'Crock Pot Pasta Never boil pasta again',
-            'Pasta Pomodoro Fresh ingredients Parmesan to taste'
-        ]
-        vectorizer = CountVectorizer()
-        count_matrix = vectorizer.fit_transform(corpus)
-        print(vectorizer.get_feature_names())
-        print(count_matrix)
 
-    # def get_feature_names(self,corpus) -> list:
-    # word_matrix = []
-    # for sentences in self:
-    # word_matrix.append(sentences.strip().split(' '))
-    # word_matr = []
-    # for i in word_matrix:
-    # word_matr = word_matr + i
-    # return list(set(word_matr))
-
-    # def fit_transform(self,corpus) -> list:
-
-    # for elements in corpus:
-    # words = elements.lower().split()
-    # mass=[]
-    # for sent in self.get_feature_names(corpus):
-    # num_words = words.count(sent)
-    # mass.append(num_words)
-    # fin.append(mass)
+if __name__ == '__main__':
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+    ]
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(corpus)
+    print(vectorizer.get_feature_names())
+    print(count_matrix)
